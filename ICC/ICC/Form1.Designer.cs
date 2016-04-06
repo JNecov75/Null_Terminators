e38264dc4f437cde430c098b87808f811a56d60e
Abdullah Busaidi <albusaia@umich.edu>
2016-04-06 18:47:24 -0400

    added a scratch ui for login

--- ICC/ICC/Form1.Designer.cs
+++ ICC/ICC/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ICC
 {
-    partial class Form1
+    partial class loginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.usernameTb = new System.Windows.Forms.TextBox();
+            this.passwordTb = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.forgotPasswordButton = new System.Windows.Forms.Button();
+            this.createAccountButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameTb
+            // 
+            this.usernameTb.Location = new System.Drawing.Point(94, 24);
+            this.usernameTb.Name = "usernameTb";
+            this.usernameTb.Size = new System.Drawing.Size(100, 22);
+            this.usernameTb.TabIndex = 0;
+            // 
+            // passwordTb
+            // 
+            this.passwordTb.Location = new System.Drawing.Point(94, 52);
+            this.passwordTb.Name = "passwordTb";
+            this.passwordTb.Size = new System.Drawing.Size(100, 22);
+            this.passwordTb.TabIndex = 1;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(12, 27);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(79, 17);
+            this.usernameLabel.TabIndex = 2;
+            this.usernameLabel.Text = "User Name";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(12, 55);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(69, 17);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(119, 80);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 30);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // forgotPasswordButton
+            // 
+            this.forgotPasswordButton.Location = new System.Drawing.Point(6, 129);
+            this.forgotPasswordButton.Name = "forgotPasswordButton";
+            this.forgotPasswordButton.Size = new System.Drawing.Size(137, 30);
+            this.forgotPasswordButton.TabIndex = 5;
+            this.forgotPasswordButton.Text = "Forgot Password";
+            this.forgotPasswordButton.UseVisualStyleBackColor = true;
+            // 
+            // createAccountButton
+            // 
+            this.createAccountButton.Location = new System.Drawing.Point(6, 165);
+            this.createAccountButton.Name = "createAccountButton";
+            this.createAccountButton.Size = new System.Drawing.Size(137, 29);
+            this.createAccountButton.TabIndex = 6;
+            this.createAccountButton.Text = "Create Account";
+            this.createAccountButton.UseVisualStyleBackColor = true;
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.createAccountButton);
+            this.Controls.Add(this.forgotPasswordButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordTb);
+            this.Controls.Add(this.usernameTb);
+            this.Name = "loginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox usernameTb;
+        private System.Windows.Forms.TextBox passwordTb;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button forgotPasswordButton;
+        private System.Windows.Forms.Button createAccountButton;
     }
 }
 
