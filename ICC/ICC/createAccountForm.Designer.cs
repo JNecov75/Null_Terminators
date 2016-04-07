9e30630cb68c2e306a5585fcae7df35b90fd28f3
Waseem Hijazi <waseemhijazi94@gmail.com>
2016-04-06 22:02:43 -0400

    Renamed Form 1 to loginForm; added a createAccount Form scratch UI

---
+++ ICC/ICC/createAccountForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ICC
+{
+    partial class createAccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fnameTb = new System.Windows.Forms.TextBox();
+            this.usernameTb = new System.Windows.Forms.TextBox();
+            this.ageTb = new System.Windows.Forms.TextBox();
+            this.emailTb = new System.Windows.Forms.TextBox();
+            this.empTypeTb = new System.Windows.Forms.TextBox();
+            this.sexTb = new System.Windows.Forms.TextBox();
+            this.lnameTb = new System.Windows.Forms.TextBox();
+            this.fnameLabel = new System.Windows.Forms.Label();
+            this.lnameLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.sexLabel = new System.Windows.Forms.Label();
+            this.empTypeLabel = new System.Windows.Forms.Label();
+            this.passwordTb = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.createAccountButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fnameTb
+            // 
+            this.fnameTb.Location = new System.Drawing.Point(68, 12);
+            this.fnameTb.Name = "fnameTb";
+            this.fnameTb.Size = new System.Drawing.Size(100, 20);
+            this.fnameTb.TabIndex = 0;
+            // 
+            // usernameTb
+            // 
+            this.usernameTb.Location = new System.Drawing.Point(269, 70);
+            this.usernameTb.Name = "usernameTb";
+            this.usernameTb.Size = new System.Drawing.Size(100, 20);
+            this.usernameTb.TabIndex = 1;
+            // 
+            // ageTb
+            // 
+            this.ageTb.Location = new System.Drawing.Point(269, 38);
+            this.ageTb.Name = "ageTb";
+            this.ageTb.Size = new System.Drawing.Size(27, 20);
+            this.ageTb.TabIndex = 2;
+            // 
+            // emailTb
+            // 
+            this.emailTb.Location = new System.Drawing.Point(68, 38);
+            this.emailTb.Name = "emailTb";
+            this.emailTb.Size = new System.Drawing.Size(138, 20);
+            this.emailTb.TabIndex = 3;
+            // 
+            // empTypeTb
+            // 
+            this.empTypeTb.Location = new System.Drawing.Point(91, 64);
+            this.empTypeTb.Name = "empTypeTb";
+            this.empTypeTb.Size = new System.Drawing.Size(24, 20);
+            this.empTypeTb.TabIndex = 4;
+            // 
+            // sexTb
+            // 
+            this.sexTb.Location = new System.Drawing.Point(343, 38);
+            this.sexTb.Name = "sexTb";
+            this.sexTb.Size = new System.Drawing.Size(26, 20);
+            this.sexTb.TabIndex = 5;
+            // 
+            // lnameTb
+            // 
+            this.lnameTb.Location = new System.Drawing.Point(269, 12);
+            this.lnameTb.Name = "lnameTb";
+            this.lnameTb.Size = new System.Drawing.Size(100, 20);
+            this.lnameTb.TabIndex = 6;
+            // 
+            // fnameLabel
+            // 
+            this.fnameLabel.AutoSize = true;
+            this.fnameLabel.Location = new System.Drawing.Point(5, 15);
+            this.fnameLabel.Name = "fnameLabel";
+            this.fnameLabel.Size = new System.Drawing.Size(57, 13);
+            this.fnameLabel.TabIndex = 7;
+            this.fnameLabel.Text = "First Name";
+            // 
+            // lnameLabel
+            // 
+            this.lnameLabel.AutoSize = true;
+            this.lnameLabel.Location = new System.Drawing.Point(205, 15);
+            this.lnameLabel.Name = "lnameLabel";
+            this.lnameLabel.Size = new System.Drawing.Size(58, 13);
+            this.lnameLabel.TabIndex = 8;
+            this.lnameLabel.Text = "Last Name";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(5, 41);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(35, 13);
+            this.emailLabel.TabIndex = 9;
+            this.emailLabel.Text = "E-mail";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(237, 41);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(26, 13);
+            this.ageLabel.TabIndex = 10;
+            this.ageLabel.Text = "Age";
+            // 
+            // sexLabel
+            // 
+            this.sexLabel.AutoSize = true;
+            this.sexLabel.Location = new System.Drawing.Point(312, 41);
+            this.sexLabel.Name = "sexLabel";
+            this.sexLabel.Size = new System.Drawing.Size(25, 13);
+            this.sexLabel.TabIndex = 11;
+            this.sexLabel.Text = "Sex";
+            // 
+            // empTypeLabel
+            // 
+            this.empTypeLabel.AutoSize = true;
+            this.empTypeLabel.Location = new System.Drawing.Point(5, 67);
+            this.empTypeLabel.Name = "empTypeLabel";
+            this.empTypeLabel.Size = new System.Drawing.Size(80, 13);
+            this.empTypeLabel.TabIndex = 12;
+            this.empTypeLabel.Text = "Employee Type";
+            // 
+            // passwordTb
+            // 
+            this.passwordTb.Location = new System.Drawing.Point(269, 96);
+            this.passwordTb.Name = "passwordTb";
+            this.passwordTb.Size = new System.Drawing.Size(100, 20);
+            this.passwordTb.TabIndex = 13;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(203, 73);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(60, 13);
+            this.usernameLabel.TabIndex = 14;
+            this.usernameLabel.Text = "User Name";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(205, 99);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 15;
+            this.passwordLabel.Text = "Password";
+            // 
+            // createAccountButton
+            // 
+            this.createAccountButton.Location = new System.Drawing.Point(144, 152);
+            this.createAccountButton.Name = "createAccountButton";
+            this.createAccountButton.Size = new System.Drawing.Size(96, 23);
+            this.createAccountButton.TabIndex = 16;
+            this.createAccountButton.Text = "Create Account";
+            this.createAccountButton.UseVisualStyleBackColor = true;
+            // 
+            // createAccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 302);
+            this.Controls.Add(this.createAccountButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordTb);
+            this.Controls.Add(this.empTypeLabel);
+            this.Controls.Add(this.sexLabel);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.lnameLabel);
+            this.Controls.Add(this.fnameLabel);
+            this.Controls.Add(this.lnameTb);
+            this.Controls.Add(this.sexTb);
+            this.Controls.Add(this.empTypeTb);
+            this.Controls.Add(this.emailTb);
+            this.Controls.Add(this.ageTb);
+            this.Controls.Add(this.usernameTb);
+            this.Controls.Add(this.fnameTb);
+            this.Name = "createAccountForm";
+            this.Text = "Create Account";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fnameTb;
+        private System.Windows.Forms.TextBox usernameTb;
+        private System.Windows.Forms.TextBox ageTb;
+        private System.Windows.Forms.TextBox emailTb;
+        private System.Windows.Forms.TextBox empTypeTb;
+        private System.Windows.Forms.TextBox sexTb;
+        private System.Windows.Forms.TextBox lnameTb;
+        private System.Windows.Forms.Label fnameLabel;
+        private System.Windows.Forms.Label lnameLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label sexLabel;
+        private System.Windows.Forms.Label empTypeLabel;
+        private System.Windows.Forms.TextBox passwordTb;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button createAccountButton;
+    }
+}
