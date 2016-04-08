636742d4cc2ee448d5d3783b6a53a72a27898e01
Waseem Hijazi <waseemhijazi94@gmail.com>
2016-04-08 03:10:17 -0400

    button modification

--- ICC/ICC/loginForm.cs
+++ ICC/ICC/loginForm.cs
@@ -77,10 +77,7 @@ namespace ICC
 
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void label2_Click(object sender, EventArgs e)
         {
